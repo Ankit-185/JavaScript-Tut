345bd50f31108e001a87037d88c6cc5c1c5ac75a
Ankit-185 <ankitchandel185@gmail.com>
2025-04-19 19:28:57 +0000

    Let Constat And Var

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "ankit@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId= 7363738
+let accountState
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
